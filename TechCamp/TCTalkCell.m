bdd8699a6254aca29331e82723e769105d149c57
Cong Vo <athanhcong@gmail.com>
2014-03-13 11:15:43 +0700

    initialize

---
+++ TechCamp/TCTalkCell.m
@@ -0,0 +1,34 @@
+//
+//  TCTalkCell.m
+//  TechCamp
+//
+//  Created by KONG on 13/3/14.
+//  Copyright (c) 2014 TechCamp. All rights reserved.
+//
+
+#import "TCTalkCell.h"
+
+@implementation TCTalkCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
